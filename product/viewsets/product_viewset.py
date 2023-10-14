b20b90855a7538da8431d772ee8e289b5ba025fd
William Wsilva <williamreis.wsilva@gmail.com>
2023-10-14 13:53:07 -0300

    config docker compose

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,15 +1,12 @@
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
     permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
 
